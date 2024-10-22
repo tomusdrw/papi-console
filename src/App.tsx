5c4fccb11e2ed9f58f00ff2a359d46725e3161fe
Victor Oliva <olivarra1@gmail.com>
2024-10-22 16:20:30 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Header } from "./pages/Header"
+import { Extrinsics } from "./pages/Extrinsics"
+
 export default function App() {
-  return <div>App</div>
+  return (
+    <div className="w-full max-w-screen-lg h-screen bg-polkadot-950 flex flex-col">
+      <Header />
+
+      <Routes>
+        <Route path="extrinsics/*" element={<Extrinsics />} />
+        <Route path="*" element={<Navigate to="/extrinsics" replace />} />
+      </Routes>
+    </div>
+  )
 }
