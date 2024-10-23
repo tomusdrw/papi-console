bb7d761ee7042f2a7654f468b29d2c188d8dbe7a
Victor Oliva <olivarra1@gmail.com>
2024-10-23 17:11:42 +0200

    cleanup json view

--- src/pages/Extrinsics/JsonMode.tsx
+++ src/pages/Extrinsics/JsonMode.tsx
@@ -10,23 +10,7 @@ export const JsonMode: FC<{
   decode: (value: Uint8Array) => unknown
   value: Uint8Array | null
 }> = ({ decode, value }) => {
-  const src = useMemo(
-    () => (value ? decode(value) : null),
-    // value
-    //   ? JSON.parse(
-    //       JSON.stringify(decode(value), (_, v) =>
-    //         typeof v === "bigint"
-    //           ? `${v}n`
-    //           : v instanceof Binary
-    //             ? bytesToString(v)
-    //             : v,
-    //       ),
-    //     )
-    //   : null,
-    [value, decode],
-  )
-
-  console.log(src)
+  const src = useMemo(() => (value ? decode(value) : null), [value, decode])
 
   return (
     <div className="overflow-auto p-2 bg-polkadot-900 text-sm">
@@ -34,11 +18,6 @@ export const JsonMode: FC<{
         src={src}
         dark
         theme="a11y"
-        // customizeNode={({ node }) => {
-        //   if (node instanceof Binary) {
-        //     return <>{node.asHex()}</>
-        //   }
-        // }}
         replacer={(v) => (v instanceof Binary ? bytesToString(v) : v)}
         customizeCopy={(v) =>
           JSON.stringify(
