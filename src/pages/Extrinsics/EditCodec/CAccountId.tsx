8439f169027edf4b168255f11ce6450cf63d6233
Victor Oliva <olivarra1@gmail.com>
2024-10-23 09:29:00 +0200

    feat: improve AccountId input

--- src/pages/Extrinsics/EditCodec/CAccountId.tsx
+++ src/pages/Extrinsics/EditCodec/CAccountId.tsx
@@ -1,10 +1,40 @@
-import {
-  SS58String,
-  getSs58AddressInfo,
-} from "@polkadot-api/substrate-bindings"
-import { EditAccountId, NOTIN } from "@codec-components"
-import { TextInputField } from "./codec-components"
+import { PolkadotIdenticon } from "@/components/PolkadotIdenticon"
 import { useSynchronizeInput } from "@/components/useSynchroniseInput"
+import { EditAccountId, NOTIN } from "@codec-components"
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react"
+import {
+  connectInjectedExtension,
+  getInjectedExtensions,
+  InjectedPolkadotAccount,
+} from "@polkadot-api/pjs-signer"
+import {
+  getSs58AddressInfo,
+  SS58String,
+} from "@polkadot-api/substrate-bindings"
+import { state, useStateObservable } from "@react-rxjs/core"
+import { combineKeys } from "@react-rxjs/utils"
+import { FC, useState } from "react"
+import {
+  concat,
+  filter,
+  finalize,
+  from,
+  fromEventPattern,
+  interval,
+  map,
+  retry,
+  startWith,
+  switchMap,
+  take,
+  timer,
+} from "rxjs"
+import { twMerge } from "tailwind-merge"
 
 export const CAccountId: EditAccountId = ({ value, onValueChanged }) => {
   const [localInput, setLocalInput] = useSynchronizeInput(
@@ -12,30 +42,167 @@ export const CAccountId: EditAccountId = ({ value, onValueChanged }) => {
     onValueChanged,
     parseValue,
   )
+  const accounts = useStateObservable(accounts$)
 
-  const invalid = getValidationError(localInput)
+  const [query, setQuery] = useState("")
+  const queryInfo = getSs58AddressInfo(query)
+  const filteredAccounts =
+    query === ""
+      ? Array.from(accounts.values())
+      : Array.from(accounts.values()).filter(
+          (account) =>
+            account.address.toLowerCase().includes(query.toLowerCase()) ||
+            (account.name &&
+              account.name.toLowerCase().includes(query.toLowerCase())),
+        )
+
   return (
-    <TextInputField
-      value={localInput ?? ""}
-      className="min-w-96"
-      onChange={setLocalInput}
-      placeholder="AccountId"
-      warn={localInput === ""}
-      softError={invalid}
-    />
+    <Combobox
+      value={localInput}
+      onChange={(v) => setLocalInput(v ?? "")}
+      onClose={() => setQuery("")}
+      immediate
+    >
+      <ComboboxButton
+        className={twMerge(
+          "text-sm border rounded p-2 border-polkadot-200 leading-tight text-white focus-within:outline",
+          "flex items-center gap-2 bg-polkadot-900",
+          value === NOTIN ? "border-orange-400" : null,
+        )}
+      >
+        {localInput && (
+          <PolkadotIdenticon
+            className="flex-shrink-0"
+            publicKey={getPublicKey(localInput)}
+            size={20}
+          />
+        )}
+        <ComboboxInput
+          className="bg-transparent outline-none w-full"
+          aria-label="AccountId"
+          displayValue={(address: string) =>
+            accounts.get(address)?.name ?? address
+          }
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </ComboboxButton>
+      <ComboboxOptions
+        anchor="bottom start"
+        className={twMerge(
+          "max-h-96 rounded border border-black p-2 bg-polkadot-800 [--anchor-gap:4px]",
+        )}
+      >
+        {filteredAccounts.length > 0 ? (
+          filteredAccounts.map((account) => (
+            <AccountOption
+              key={account.address}
+              account={account.address}
+              publicKey={getPublicKey(account.address)}
+            />
+          ))
+        ) : queryInfo.isValid ? (
+          <AccountOption account={query} publicKey={queryInfo.publicKey} />
+        ) : (
+          <div className="text-slate-400">
+            The value is not a valid Account ID
+          </div>
+        )}
+      </ComboboxOptions>
+    </Combobox>
   )
+}
+
+const AccountOption: FC<{ account: string; publicKey: Uint8Array | null }> = ({
+  account,
+  publicKey,
+}) => {
+  const details = useStateObservable(accountDetail$(account))
+
+  return (
+    <ComboboxOption
+      value={account}
+      className="data-[focus]:bg-polkadot-700 cursor-pointer flex flex-row items-center gap-2 p-1 rounded overflow-hidden"
+    >
+      <PolkadotIdenticon
+        className="flex-shrink-0"
+        publicKey={publicKey}
+        size={32}
+      />
+      <div className="flex flex-col justify-center text-white leading-tight overflow-hidden">
+        {details?.name && <span>{details.name}</span>}
+        <span className="text-slate-400 text-ellipsis overflow-hidden">
+          {account}
+        </span>
+      </div>
+    </ComboboxOption>
+  )
+}
+
+const getPublicKey = (address: string) => {
+  const info = getSs58AddressInfo(address)
+  return info.isValid ? info.publicKey : null
 }
 
 const getValidationError = (value: string) => {
   if (value === "") return null
-  const info = getSs58AddressInfo(value)
-  return info.isValid &&
-    (info.publicKey.length === 32 || info.publicKey.length === 32)
-    ? null
-    : "Invalid address"
+  const { isValid } = getSs58AddressInfo(value)
+  return isValid ? null : "Invalid address"
 }
 const parseValue = (value: string): SS58String | NOTIN => {
   if (value.trim() === "") return NOTIN
   if (getValidationError(value)) return NOTIN
   return value
 }
+
+const extensions$ = state(
+  concat(
+    timer(0, 100).pipe(
+      map(getInjectedExtensions),
+      filter((v) => v.length > 0),
+      take(1),
+    ),
+    interval(2000).pipe(map(getInjectedExtensions)),
+  ),
+  [],
+)
+const extensionAccounts$ = combineKeys(extensions$, (extension) =>
+  from(connectInjectedExtension(extension)).pipe(
+    switchMap((extension) =>
+      fromEventPattern<InjectedPolkadotAccount[]>(
+        extension.subscribe,
+        (_, fn) => fn(),
+      ).pipe(
+        startWith(extension.getAccounts()),
+        finalize(extension.disconnect),
+      ),
+    ),
+    retry({
+      delay: 100,
+    }),
+  ),
+)
+const accounts$ = state(
+  extensionAccounts$.pipe(
+    map(
+      (extensionAccounts) =>
+        new Map(
+          Array.from(extensionAccounts.entries()).flatMap(
+            ([extension, accounts]) =>
+              accounts.map((account) => [
+                account.address,
+                {
+                  ...account,
+                  extension,
+                },
+              ]),
+          ),
+        ),
+    ),
+  ),
+  new Map<string, InjectedPolkadotAccount & { extension: string }>(),
+)
+
+const accountDetail$ = state(
+  (account: string) => accounts$.pipe(map((v) => v.get(account) ?? null)),
+  null,
+)
