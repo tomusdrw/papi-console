abd89b27708927573fa7c6d557d89d0b8cb83ce5
Victor Oliva <olivarra1@gmail.com>
2024-10-23 11:39:58 +0200

    Move binary display out from edit mode, add binary edit button to extrinsic

--- src/pages/Extrinsics/EditMode.tsx
+++ src/pages/Extrinsics/EditMode.tsx
@@ -1,25 +1,23 @@
-import { FC, useEffect, useRef, useState } from "react"
-import { metadata, Binary, HexString } from "@polkadot-api/substrate-bindings"
-import { EditCodec } from "./EditCodec"
-import { TreeCodec } from "./EditCodec/Tree/index"
 import {
-  CodecComponentValue,
-  CodecComponentType,
   CodecComponentUpdate,
+  CodecComponentValue,
   MetadataType,
 } from "@codec-components"
-import { SubtreeFocus } from "./common/SubtreeFocus"
-import { Circle } from "lucide-react"
-import { synchronizeScroll } from "./common/scroll"
+import { useEffect, useRef, useState } from "react"
 import { MarkersContextProvider, VisibleWindow } from "./common/Markers"
-import { CopyText } from "@/components/Copy"
+import { synchronizeScroll } from "./common/scroll"
+import { SubtreeFocus } from "./common/SubtreeFocus"
+import { EditCodec } from "./EditCodec"
+import { TreeCodec } from "./EditCodec/Tree/index"
 import { FocusPath } from "./FocusPath"
 
 export const EditMode: React.FC<{
   codecType: number
   metadata: MetadataType
-  initialValue?: HexString
-}> = ({ codecType, initialValue, metadata: metadataDecoded }) => {
+  value: CodecComponentValue
+  onUpdate: (value: CodecComponentUpdate) => void
+}> = (props) => {
+  const { metadata, codecType, value } = props
   const treeRef = useRef<HTMLDivElement | null>(null)
   const listRef = useRef<HTMLDivElement | null>(null)
 
@@ -28,43 +26,12 @@ export const EditMode: React.FC<{
     return synchronizeScroll(listRef.current, treeRef.current)
   }, [])
 
-  // interior X1 AccountId doesn't mark as valid??
-
-  const [innerValue, setInnerValue] = useState<CodecComponentValue>({
-    type: CodecComponentType.Initial,
-    value: initialValue,
-  })
   const [focusingSubtree, setFocusingSubtree] = useState<string[] | null>(null)
 
-  const props = {
-    metadata: metadataDecoded as any,
-    codecType: codecType,
-    value: innerValue,
-    onUpdate: (value: CodecComponentUpdate) => {
-      setInnerValue({ type: CodecComponentType.Updated, value })
-    },
-  }
-
-  useEffect(() => {
-    setInnerValue({
-      type: CodecComponentType.Initial,
-      value: initialValue,
-    })
-  }, [metadata, codecType])
-
   return (
-    <div className="flex flex-col items-start w-full max-w-screen-lg text-polkadot-100 overflow-hidden bg-polkadot-900">
-      <BinaryDisplay
-        value={
-          (innerValue.type === CodecComponentType.Initial
-            ? innerValue.value
-            : innerValue.value.empty
-              ? null
-              : innerValue.value.encoded) ?? null
-        }
-      />
+    <div className="flex flex-col items-start overflow-hidden">
       <FocusPath
-        metadata={metadataDecoded as any}
+        metadata={metadata}
         typeId={codecType}
         value={focusingSubtree}
         onFocus={setFocusingSubtree}
@@ -86,7 +53,7 @@ export const EditMode: React.FC<{
               className="w-96 sticky top-0 pl-2 pb-16 leading-loose overflow-hidden max-sm:hidden"
             >
               <div className="relative">
-                {!innerValue.value ? (
+                {!value.value ? (
                   <div className="text-sm text-slate-400">(Empty)</div>
                 ) : (
                   <TreeCodec {...props} />
@@ -95,53 +62,13 @@ export const EditMode: React.FC<{
               </div>
             </div>
             <div className="flex-1">
-              <div className="p-2 rounded bg-[#041424]">
+              <div className="p-2 rounded bg-polkadot-900">
                 <EditCodec {...props} />
               </div>
             </div>
           </div>
         </MarkersContextProvider>
       </SubtreeFocus>
-    </div>
-  )
-}
-
-const DISPLAY_MAX_LEN = 1024
-const COPY_MAX_LEN = 5 * 1024 * 1024
-const BinaryDisplay: FC<{ value: Uint8Array | HexString | null }> = ({
-  value,
-}) => {
-  const displayLength = DISPLAY_MAX_LEN * 2 + 2
-
-  const copyLength = COPY_MAX_LEN * 2 + 2
-  const hex = value
-    ? typeof value === "string"
-      ? value.slice(0, copyLength)
-      : Binary.fromBytes(value.slice(0, COPY_MAX_LEN)).asHex()
-    : null
-  const exceedsDisplayLength = hex && hex.length > displayLength
-  const exceedsCopyLength = !!hex && hex.length >= COPY_MAX_LEN * 2 + 2
-
-  const copyDisabled = hex === null || exceedsCopyLength
-
-  return (
-    <div className="p-2 w-full">
-      <div className="px-3 py-2 gap-2 rounded flex flex-row items-center bg-polkadot-800">
-        <CopyText text={hex ?? ""} disabled={copyDisabled} />
-        <div className="text-sm tabular-nums max-h-12 overflow-hidden whitespace-nowrap text-ellipsis">
-          {hex ? (
-            hex.slice(0, displayLength) + (exceedsDisplayLength ? "…" : "")
-          ) : (
-            <div className="flex flex-row items-center gap-1">
-              <span>Cannot display hex. Complete required values shown by</span>{" "}
-              <Circle size={8} strokeWidth={4} className="text-red-600" />
-              (incomplete), or
-              <Circle size={8} strokeWidth={4} className="text-orange-600" />
-              (partially incomplete)
-            </div>
-          )}
-        </div>
-      </div>
     </div>
   )
 }
--- src/pages/Extrinsics/Extrinsics.tsx
+++ src/pages/Extrinsics/Extrinsics.tsx
@@ -1,6 +1,19 @@
 import { metadata$ } from "@/chain.state"
+import { CopyText } from "@/components/Copy"
+import { BinaryEdit } from "@/components/Icons"
+import { withSubscribe } from "@/components/withSuspense"
+import {
+  CodecComponentType,
+  CodecComponentValue,
+  NOTIN,
+} from "@codec-components"
+import { getDynamicBuilder, getLookupFn } from "@polkadot-api/metadata-builders"
+import { Binary, HexString } from "@polkadot-api/substrate-bindings"
 import { state, useStateObservable } from "@react-rxjs/core"
+import { FC, useState } from "react"
 import { map } from "rxjs"
+import { twMerge } from "tailwind-merge"
+import { BinaryEditModal } from "./BinaryEditModal"
 import { EditMode } from "./EditMode"
 
 const extrinsicProps$ = state(
@@ -14,23 +27,125 @@ const extrinsicProps$ = state(
             metadata.extrinsic.type,
     })),
   ),
-  null,
+)
+const extrinsicDecoder = extrinsicProps$.pipeState(
+  map(
+    ({ metadata, codecType }) =>
+      getDynamicBuilder(getLookupFn(metadata)).buildDefinition(codecType).dec,
+  ),
 )
 
-export const Extrinsics = () => {
+export const Extrinsics = withSubscribe(() => {
   const extrinsicProps = useStateObservable(extrinsicProps$)
+  const decoder = useStateObservable(extrinsicDecoder)
 
-  if (!extrinsicProps) {
-    return <div>Loading metadata...</div>
-  }
+  const [componentValue, setComponentValue] = useState<CodecComponentValue>({
+    type: CodecComponentType.Initial,
+    value:
+      "0x630b040101010031323334353637383934353631363531363531363531363531363531353631350100000108000100002c00000000080000000000",
+  })
+  const binaryValue =
+    (componentValue.type === CodecComponentType.Initial
+      ? componentValue.value
+      : componentValue.value.empty
+        ? null
+        : componentValue.value.encoded) ?? null
 
   return (
     <div className="flex flex-col overflow-hidden">
       <div>Extrinsics</div>
+
+      <BinaryDisplay
+        value={binaryValue}
+        isEmpty={
+          componentValue.type === CodecComponentType.Initial &&
+          componentValue.value == null
+        }
+        decode={(value) => {
+          try {
+            return decoder(value)
+          } catch (_) {
+            return NOTIN
+          }
+        }}
+        onValueChanged={(decoded) => {
+          setComponentValue({
+            type: CodecComponentType.Updated,
+            value: { empty: false, decoded },
+          })
+          return true
+        }}
+      />
+
       <EditMode
         {...extrinsicProps}
-        initialValue="0x630b040101010031323334353637383934353631363531363531363531363531363531353631350100000108000100002c00000000080000000000"
+        value={componentValue}
+        onUpdate={(value) =>
+          setComponentValue({ type: CodecComponentType.Updated, value })
+        }
       />
+    </div>
+  )
+})
+
+const DISPLAY_MAX_LEN = 1024
+const COPY_MAX_LEN = 5 * 1024 * 1024
+const BinaryDisplay: FC<{
+  value: Uint8Array | HexString | null
+  isEmpty: boolean
+  onValueChanged: (value: any | NOTIN) => boolean
+  decode: (value: Uint8Array | HexString) => any | NOTIN
+}> = ({ value, isEmpty, onValueChanged, decode }) => {
+  const [binaryOpen, setBinaryOpen] = useState(false)
+
+  const displayLength = DISPLAY_MAX_LEN * 2 + 2
+
+  const copyLength = COPY_MAX_LEN * 2 + 2
+  const hex = value
+    ? typeof value === "string"
+      ? value.slice(0, copyLength)
+      : Binary.fromBytes(value.slice(0, COPY_MAX_LEN)).asHex()
+    : null
+  const exceedsDisplayLength = hex && hex.length > displayLength
+  const exceedsCopyLength = !!hex && hex.length >= COPY_MAX_LEN * 2 + 2
+
+  const copyDisabled = hex === null || exceedsCopyLength
+
+  return (
+    <div className="p-2 w-full">
+      <div className="px-3 py-2 gap-2 rounded flex flex-row items-center bg-polkadot-800">
+        <CopyText text={hex ?? ""} disabled={copyDisabled} />
+        <div className="text-sm tabular-nums max-h-12 overflow-hidden whitespace-nowrap text-ellipsis flex-1">
+          {hex ? (
+            hex.slice(0, displayLength) + (exceedsDisplayLength ? "…" : "")
+          ) : (
+            <div className="flex flex-row items-center gap-1 text-slate-400">
+              {isEmpty
+                ? "Start by filling out your extrinsic, or enter a binary using the edit binary button at the end of this line."
+                : "Cannot display hex. Complete the required values."}
+            </div>
+          )}
+        </div>
+        <BinaryEdit
+          size={24}
+          className={twMerge("cursor-pointer hover:text-polkadot-300")}
+          onClick={() => setBinaryOpen(true)}
+        />
+        <BinaryEditModal
+          status={{
+            encodedValue:
+              typeof value === "string"
+                ? Binary.fromHex(value).asBytes()
+                : (value ?? undefined),
+            onValueChanged,
+            decode,
+            type: "complete",
+          }}
+          open={binaryOpen}
+          path=""
+          onClose={() => setBinaryOpen(false)}
+        />
+      </div>
     </div>
   )
 }
